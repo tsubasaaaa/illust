4835fa8e62d05cace4f20d4c0c84230022e5aedd
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-06-03 13:55:31 +0900

    model updateillustrationscontroller

---
+++ app/models/illustration.rb
@@ -0,0 +1,4 @@
+class Illustration < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_122355) do
+ActiveRecord::Schema.define(version: 2019_06_03_043041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "illustrations", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.text "text"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_illustrations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_06_02_122355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "illustrations", "users"
 end

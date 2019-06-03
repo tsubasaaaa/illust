30222ca5bd011e08494dfeebec85f04b0ff876dd
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-06-03 17:15:01 +0900

    update comments users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def show
+    user = user.find(params[:id])
+    @name = user.name
+    @illustrations = user.illustrations.page(params[:page]).per(5).order("created_at DESC")
+  end
 end

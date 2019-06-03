b7a4c3c81dade0c18e95e7032e89c5a49814460b
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-06-03 16:42:46 +0900

    create commentmodel

--- db/migrate/20190603043041_create_illustrations.rb
+++ db/migrate/20190603043041_create_illustrations.rb
@@ -1,9 +1,9 @@
 class CreateIllustrations < ActiveRecord::Migration[5.2]
   def change
     create_table :illustrations do |t|
-      t.string :title
-      t.string :image
-      t.text   :text
+      t.string     :title
+      t.string     :image
+      t.text       :text
       t.references :user, foreign_key: true
       t.timestamps
     end
